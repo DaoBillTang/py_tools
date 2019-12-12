588dfe0aa5c0c6f843fd730381ff6122d3869b27
tangl <tangl@aplus-it.cn>
2019-12-12 16:26:53 +0800

    base 0.4.7.17 brief 0.4.7.17 model 0.3.1.2 内容： 1. 将 action code list 和 action code range 都改为了 csv 文件 2. 改名： xxx_api -> api xxx; 方便寻找； 3. 删除了过时 内容 ComputeInfo 4. 删除了 ComputeTool 已标记的过时方法 5. 分析结果 中 添加了 对数据有效性的校验，并抛出异常 6. 补充了 部分地方的 注释文档 7. 删除了 ComputerModel 中的 info class,并替换到 ComputerTool 8. 原始的 IntegrationSeniorFunTwo 标记为 不可用；并且 分析模式 默认值改为 IntegrationSeniorFunOne； 9. 修改了部分地方的 错误日志 10. 错误日志 添加了当前的程序的运行模式：     create model;instant,appraise,wait,exit,init 11. 修改了些bug 12. 添加了A_027 13. build tool 更新 14. 更新 A_006,A_009 15. fix bug :提交的 score 错误的问题 16. 代码 格式化 17. 添加了 新的一种 计算肘关节内外数值的方案 18. 对于 base api 添加了功能，可以同时包含多个方向进行协同运算了 19. 添加了 新的语音 20. 修改 耸肩 的计算方案;修改了 肘关节内外的 -> 拥有 正负效果 21. 优化了 计算过程的 效率，减少了部分中间过程 22. 修改了 手距离身体距离

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def setup():
     print("start")
     setup(
         name="dtb_tools",
-        version="0.0.11",
+        version="0.0.12",
         keywords=("pytools container"),
         description="python tools",
         long_description="python tools",
